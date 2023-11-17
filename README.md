8c23b3ac425b71162278068fc66aba7450442d44
Frank Kong <frkong@redhat.com>
2023-11-17 11:58:02 -0500

    chore: add simple proxy server for ngrok

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # test-repo-3
+Proxy for `ngrok` to skip warning screen. Set `NGROK_HOST` environmental variable to url of the `ngrok` host.
---
+++ index.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const { createProxyMiddleware } = require("http-proxy-middleware");
+
+const app = express();
+const defaultTargetUrl = "http://www.example.com";
+const targetUrl = process.env.NGROK_HOST || defaultTargetUrl; // Replace with ngrok host url
+
+// Create a proxy middleware
+const apiProxy = createProxyMiddleware({
+  target: targetUrl,
+  changeOrigin: true, // Necessary for the target API to receive requests from your proxy
+  onProxyReq: (proxyReq, req, res) => {
+    // Log the incoming request
+    console.log(`Request received: ${req.method} ${req.originalUrl}`);
+  },
+  onProxyRes: (proxyRes, req, res) => {
+    // Append additional header to the response
+    proxyRes.headers["ngrok-skip-browser-warning"] = "69420";
+  },
+});
+
+// Use the proxy middleware for all routes
+app.use("*", apiProxy);
+
+// Start the server
+const port = 5555; // Choose a port for your proxy server
+app.listen(port, () => {
+  console.log(`Proxy server listening on port ${port}`);
+});
